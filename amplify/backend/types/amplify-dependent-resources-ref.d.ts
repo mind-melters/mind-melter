54fae22315e08934ecd996b55a1f7a4a6506ab5f
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-06-16 17:12:00 -0700

    Initialize Project and Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mindmelters": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "mindmelters7198d08f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ app/src/main/java/com/mca/mindmelter/MindMelterApplication.java
@@ -0,0 +1,27 @@
+package com.mca.mindmelter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+
+public class MindMelterApplication extends Application {
+    public static final String TAG = "mindmelterapplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        try {
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i(TAG, "Initialized Amplify");
+        } catch (AmplifyException ae) {
+            Log.e(TAG, "Error initializing Amplify: " + ae.getMessage(), ae);
+        }
+    }
+}
