ccd9d883fc6a42eecc65d00d203c3ef8ced34d83
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-06-20 14:24:46 -0700

    Update ChatActivity for superfluous code

--- app/src/main/java/com/mca/mindmelter/activities/ChatActivity.java
+++ app/src/main/java/com/mca/mindmelter/activities/ChatActivity.java
@@ -1,22 +1,17 @@
 package com.mca.mindmelter.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.EditText;
 
 import com.mca.mindmelter.R;
 import com.mca.mindmelter.adapters.ChatAdapter;
 import com.mca.mindmelter.viewmodels.ChatViewModel;
-import com.theokanning.openai.completion.chat.ChatMessage;
-
-import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
     private ChatViewModel viewModel;
@@ -37,25 +32,17 @@ public class ChatActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Observe LiveData from ViewModel
-        viewModel.getChatMessagesLiveData().observe(this, new Observer<List<ChatMessage>>() {
-            @Override
-            public void onChanged(List<ChatMessage> chatMessages) {
-                chatAdapter.submitList(chatMessages); // Update UI with new data
-            }
-        });
+        viewModel.getChatMessagesLiveData().observe(this, chatMessages -> chatAdapter.submitList(chatMessages));
 
         // Handle message sending
-        EditText inputText = findViewById(R.id.input_text);
-        Button sendButton = findViewById(R.id.send_button);
+        EditText inputText = findViewById(R.id.message_input);
+        ImageButton sendButton = findViewById(R.id.send_message_button);
 
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String userMessage = inputText.getText().toString();
-                if (!userMessage.isEmpty()) {
-                    viewModel.sendMessage(userMessage);
-                    inputText.setText("");
-                }
+        sendButton.setOnClickListener(view -> {
+            String userMessage = inputText.getText().toString();
+            if (!userMessage.isEmpty()) {
+                viewModel.sendMessage(userMessage);
+                inputText.setText("");
             }
         });
     }
--- app/src/main/java/com/mca/mindmelter/adapters/ChatAdapter.java
+++ app/src/main/java/com/mca/mindmelter/adapters/ChatAdapter.java
@@ -6,37 +6,47 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mca.mindmelter.R;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 
-import java.util.List;
-
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
-    private List<ChatMessage> chatMessages;
-
+public class ChatAdapter extends ListAdapter<ChatMessage, ChatAdapter.ChatViewHolder> {
     private static final int VIEW_TYPE_USER = 0;
     private static final int VIEW_TYPE_ASSISTANT = 1;
 
-    public ChatAdapter(List<ChatMessage> chatMessages) {
-        this.chatMessages = chatMessages;
+    public ChatAdapter() {
+        super(DIFF_CALLBACK);
     }
 
-    public void setChatMessages(List<ChatMessage> chatMessages) {
-        this.chatMessages = chatMessages;
-    }
+    static final DiffUtil.ItemCallback<ChatMessage> DIFF_CALLBACK =
+            new DiffUtil.ItemCallback<ChatMessage>() {
+                @Override
+                public boolean areItemsTheSame(@NonNull ChatMessage oldChatMessage, @NonNull ChatMessage newChatMessage) {
+                    // You may need to provide a better way of comparing ChatMessage objects.
+                    // The following is just an example assuming ChatMessage has an ID field.
+                    //return oldChatMessage.getId().equals(newChatMessage.getId());
+                    return oldChatMessage.equals(newChatMessage);
+                }
+
+                @Override
+                public boolean areContentsTheSame(@NonNull ChatMessage oldChatMessage, @NonNull ChatMessage newChatMessage) {
+                    return oldChatMessage.equals(newChatMessage);
+                }
+            };
 
     @Override
     public int getItemViewType(int position) {
-        ChatMessage message = chatMessages.get(position);
+        ChatMessage message = getItem(position);
         return message.getRole().equals(ChatMessageRole.USER.value()) ? VIEW_TYPE_USER : VIEW_TYPE_ASSISTANT;
     }
 
     @NonNull
     @Override
-    public ChatViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
         if (viewType == VIEW_TYPE_USER) {
             view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_chat_message_item_user, parent, false);
@@ -48,13 +58,8 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
-        ChatMessage chatMessage = chatMessages.get(position);
+        ChatMessage chatMessage = getItem(position);
         holder.bind(chatMessage);
-    }
-
-    @Override
-    public int getItemCount() {
-        return chatMessages.size();
     }
 
     static class ChatViewHolder extends RecyclerView.ViewHolder {
