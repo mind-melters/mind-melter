a02b8f0700754ea551b409f527ba746083ff90a6
Alex Carr <mralex.carr@yahoo.com>
2023-06-22 10:59:05 -0700

    User Verified

--- app/src/main/java/com/mca/mindmelter/MainActivity.java
+++ app/src/main/java/com/mca/mindmelter/MainActivity.java
@@ -4,46 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import com.amplifyframework.core.Amplify;
 import com.mca.mindmelter.activities.ChatActivity;
 
 import com.mca.mindmelter.activities.ProfilePageActivity;
 import com.mca.mindmelter.activities.TriviaActivity;
+import com.mca.mindmelter.activities.authentication.LogInActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        setUpProfilePageButton();
-        setUpChatActivityButton();
-
-        // This will automatically reroute to the TriviaActivity on App load
-        Intent goToTriviaActivityIntent = new Intent(MainActivity.this, TriviaActivity.class);
-        startActivity(goToTriviaActivityIntent);
-    }
-
-    public void setUpProfilePageButton() {
-        findViewById(R.id.activity_trivia_learn_more_button).setOnClickListener(view -> {
-            Intent goToSettingsPageIntent = new Intent(MainActivity.this, ProfilePageActivity.class);
-            startActivity(goToSettingsPageIntent);
-        });
-    }
-
-    public void setUpChatActivityButton() {
-        Button btnGotoChat = findViewById(R.id.btn_goto_chat);
-        btnGotoChat.setOnClickListener(view -> {
-                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
-
-                // Add your TriviaId as an extra to the intent
-                String triviaId = "241b57fb-02b3-4314-9748-20fe2652e7b0";  // replace with actual triviaId
-                intent.putExtra("triviaId", triviaId);
-
-                // Launch the ChatActivity
-                startActivity(intent);
-        });
+        Amplify.Auth.getCurrentUser(
+                success -> {
+                    Log.i(TAG, "User authenticated with username: " + success.getUsername());
+                    // if the user is authenticated, start the TriviaActivity
+                    startActivity(new Intent(MainActivity.this, TriviaActivity.class));
+                },
+                failure -> {
+                    Log.i(TAG, "There is no current authenticated user");
+                    // if the user is not authenticated, start the LogInActivity
+                    startActivity(new Intent(MainActivity.this, LogInActivity.class));
+                }
+        );
+        finish();
     }
 }
