6aef90f32c69f78f2a6aa182e5d7db12e37d02e4
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-06-22 21:28:43 -0700

    Finish trivial pursuit

--- app/src/main/java/com/mca/mindmelter/activities/GenerateTriviaActivity.java
+++ app/src/main/java/com/mca/mindmelter/activities/GenerateTriviaActivity.java
@@ -6,49 +6,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mca.mindmelter.R;
 import com.mca.mindmelter.enums.TriviaCategory;
 import com.ramotion.circlemenu.CircleMenuView;
 
 public class GenerateTriviaActivity extends AppCompatActivity {
-
+    private TextView artsAndLiteratureLabel;
+    private TextView scienceAndNatureLabel;
+    private TextView sportsAndLeisureLabel;
+    private TextView historyLabel;
+    private TextView geographyLabel;
+    private TextView entertainmentLabel;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_trivia);
 
+        // Initialize views
+        artsAndLiteratureLabel = findViewById(R.id.arts_and_literature_label);
+        scienceAndNatureLabel = findViewById(R.id.science_and_nature_lable);
+        sportsAndLeisureLabel = findViewById(R.id.sports_and_leisure_label);
+        historyLabel = findViewById(R.id.history_label);
+        geographyLabel = findViewById(R.id.geography_label);
+        entertainmentLabel = findViewById(R.id.entertainment_label);
+        
         final CircleMenuView menu = findViewById(R.id.circle_menu);
         menu.setEventListener(new CircleMenuView.EventListener() {
             @Override
             public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
-                Log.d("D", "onMenuOpenAnimationStart");
             }
 
             @Override
             public void onMenuOpenAnimationEnd(@NonNull CircleMenuView view) {
-                Log.d("D", "onMenuOpenAnimationEnd");
+                artsAndLiteratureLabel.setVisibility(View.VISIBLE);
+                scienceAndNatureLabel.setVisibility(View.VISIBLE);
+                sportsAndLeisureLabel.setVisibility(View.VISIBLE);
+                historyLabel.setVisibility(View.VISIBLE);
+                geographyLabel.setVisibility(View.VISIBLE);
+                entertainmentLabel.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onMenuCloseAnimationStart(@NonNull CircleMenuView view) {
-                Log.d("D", "onMenuCloseAnimationStart");
+                artsAndLiteratureLabel.setVisibility(View.INVISIBLE);
+                scienceAndNatureLabel.setVisibility(View.INVISIBLE);
+                sportsAndLeisureLabel.setVisibility(View.INVISIBLE);
+                historyLabel.setVisibility(View.INVISIBLE);
+                geographyLabel.setVisibility(View.INVISIBLE);
+                entertainmentLabel.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onMenuCloseAnimationEnd(@NonNull CircleMenuView view) {
-                Log.d("D", "onMenuCloseAnimationEnd");
             }
 
             @Override
             public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int index) {
-                Log.d("D", "onButtonClickAnimationStart| index: " + index);
+                artsAndLiteratureLabel.setVisibility(View.INVISIBLE);
+                scienceAndNatureLabel.setVisibility(View.INVISIBLE);
+                sportsAndLeisureLabel.setVisibility(View.INVISIBLE);
+                historyLabel.setVisibility(View.INVISIBLE);
+                geographyLabel.setVisibility(View.INVISIBLE);
+                entertainmentLabel.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onButtonClickAnimationEnd(@NonNull CircleMenuView view, int index) {
-                Log.d("D", "onButtonClickAnimationEnd| index: " + index);
-
                 TriviaCategory category = TriviaCategory.values()[index];
                 Intent intent = new Intent(GenerateTriviaActivity.this, TriviaActivity.class);
                 intent.putExtra("CATEGORY", category);
