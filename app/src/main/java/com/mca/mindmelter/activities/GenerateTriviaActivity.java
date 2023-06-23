09efd25d31ebdd15058807b0f6c90c05bb9804d2
Matthew Austin <111619012+matthew-c-austin@users.noreply.github.com>
2023-06-22 18:52:58 -0700

    Create GenerateTriviaActivity

---
+++ app/src/main/java/com/mca/mindmelter/activities/GenerateTriviaActivity.java
@@ -0,0 +1,16 @@
+package com.mca.mindmelter.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mca.mindmelter.R;
+
+public class GenerateTriviaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_trivia);
+    }
+}
